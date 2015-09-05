b47ff36863364eb0bee048671fea90f47296308b
Tim McKague <tim.mckague@loblaw.ca>
2015-09-05 13:25:23 -0400

    analytics async

--- js/analytics.js
+++ js/analytics.js
@@ -1,7 +1,10 @@
-(function(w,g){w['GoogleAnalyticsObject']=g;
-w[g]=w[g]||function(){(w[g].q=w[g].q||[]).push(arguments)};w[g].l=1*new Date();})(window,'ga');
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
 // Optional TODO: replace with your Google Analytics profile ID.
 ga('create', 'UA-XXXX-Y');
 ga('send', 'pageview');
+
 
